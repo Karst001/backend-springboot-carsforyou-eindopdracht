9c2816c5e24436cf175f847ad68f58a199f0abda
Karst001 <ad.karsten@outlook.com>
2025-12-28 12:25:33 +0100

    created serviceorder-parts response DTO

--- src/main/java/nl/carsforyou/garage/dtos/ServiceOrder/ServiceOrderPartResponseDto.java
+++ src/main/java/nl/carsforyou/garage/dtos/ServiceOrder/ServiceOrderPartResponseDto.java
@@ -1,5 +1,61 @@
 // Used to return data back to client, contains read-only and calculated fields that are safe to return
 package nl.carsforyou.garage.dtos.ServiceOrder;
 
+import java.math.BigDecimal;
+
 public class ServiceOrderPartResponseDto {
+    private Long serviceOrderPartId;
+    private Long serviceId;                 //Foreign key to ServiceOrder
+    private Long partId;                    //Foreign key to Parts
+    private BigDecimal unitCost;            //history unitCost
+    private BigDecimal unitPrice;           //history unitPrice
+    private int qtyUsed;
+
+    public Long getServiceOrderPartId() {
+        return serviceOrderPartId;
+    }
+
+    public void setServiceOrderPartId(Long serviceOrderPartId) {
+        this.serviceOrderPartId = serviceOrderPartId;
+    }
+
+    public Long getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(Long serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public Long getPartId() {
+        return partId;
+    }
+
+    public void setPartId(Long partId) {
+        this.partId = partId;
+    }
+
+    public BigDecimal getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(BigDecimal unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQtyUsed() {
+        return qtyUsed;
+    }
+
+    public void setQtyUsed(int qtyUsed) {
+        this.qtyUsed = qtyUsed;
+    }
 }
