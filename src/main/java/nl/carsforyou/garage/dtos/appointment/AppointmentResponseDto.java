b5d657e9677db27b78b56546536fdc45612750c3
Karst001 <ad.karsten@outlook.com>
2025-12-28 11:46:22 +0100

    created appointment respoinse DTO

--- src/main/java/nl/carsforyou/garage/dtos/appointment/AppointmentResponseDto.java
+++ src/main/java/nl/carsforyou/garage/dtos/appointment/AppointmentResponseDto.java
@@ -1,5 +1,61 @@
 // Used to return data back to client, contains read-only and calculated fields that are safe to return
 package nl.carsforyou.garage.dtos.appointment;
 
+import java.time.LocalDateTime;
+
 public class AppointmentResponseDto {
+    private Long appointmentId;
+    private LocalDateTime appointmentDate;
+    private String reasonForVisit;
+    private LocalDateTime completedDate;
+    private Long vehicleId;
+    private Long createdByUserId;
+
+    public Long getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(Long appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public LocalDateTime getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDateTime appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getReasonForVisit() {
+        return reasonForVisit;
+    }
+
+    public void setReasonForVisit(String reasonForVisit) {
+        this.reasonForVisit = reasonForVisit;
+    }
+
+    public LocalDateTime getCompletedDate() {
+        return completedDate;
+    }
+
+    public void setCompletedDate(LocalDateTime completedDate) {
+        this.completedDate = completedDate;
+    }
+
+    public Long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public Long getCreatedByUserId() {
+        return createdByUserId;
+    }
+
+    public void setCreatedByUserId(Long createdByUserId) {
+        this.createdByUserId = createdByUserId;
+    }
 }
