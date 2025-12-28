40f8d622812bec49fb78375f133f7d2617a2d23b
Karst001 <ad.karsten@outlook.com>
2025-12-28 12:19:34 +0100

    created serviceorder response DTO

--- src/main/java/nl/carsforyou/garage/dtos/ServiceOrder/ServiceOrderResponseDto.java
+++ src/main/java/nl/carsforyou/garage/dtos/ServiceOrder/ServiceOrderResponseDto.java
@@ -1,5 +1,34 @@
 // Used to return data back to client, contains read-only and calculated fields that are safe to return
 package nl.carsforyou.garage.dtos.ServiceOrder;
 
+import java.time.LocalDateTime;
+
 public class ServiceOrderResponseDto {
+    private Long serviceOrderId;
+    private LocalDateTime serviceCompletedDate;
+    private Long vehicleId;
+
+    public Long getServiceOrderId() {
+        return serviceOrderId;
+    }
+
+    public void setServiceOrderId(Long serviceOrderId) {
+        this.serviceOrderId = serviceOrderId;
+    }
+
+    public LocalDateTime getServiceCompletedDate() {
+        return serviceCompletedDate;
+    }
+
+    public void setServiceCompletedDate(LocalDateTime serviceCompletedDate) {
+        this.serviceCompletedDate = serviceCompletedDate;
+    }
+
+    public Long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
 }
